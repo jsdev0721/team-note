1190827d6aba16fda30529ede681a08dac03a7b6
sweetsbox <sweetsbox1992@gmail.com>
2024-08-05 16:45:55 +0900

    0805 문성경
    Department 수정
    Approval 수정
    ApprovalFile 삭제
    File pakage 추가

---
+++ src/main/java/com/groupware/note/files/FileRepository.java
@@ -0,0 +1,10 @@
+package com.groupware.note.files;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface FileRepository extends JpaRepository<Files, Integer> {
+	List<Files> findByStoreFileName(String storeFileName);
+}
