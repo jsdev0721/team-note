d782c79407980b4e3e3343c83fc47d41bb39994f
estelsn <cjsdltmdgh@naver.com>
2024-08-06 12:36:29 +0900

    0806 이승호 부서데이터
    부서데이터 test에 만들어뒀음. 다운받고 junit test 돌리고 서버 실행해주세요

---
+++ src/main/java/com/groupware/note/position/PositionsRepository.java
@@ -0,0 +1,7 @@
+package com.groupware.note.position;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PositionsRepository extends JpaRepository<Positions, Integer>{
+
+}
--- src/test/java/com/groupware/note/NoteApplicationTests.java
+++ src/test/java/com/groupware/note/NoteApplicationTests.java
@@ -1,13 +1,80 @@
 package com.groupware.note;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.groupware.note.department.DepartmentRepository;
+import com.groupware.note.department.DepartmentService;
+import com.groupware.note.department.Departments;
+import com.groupware.note.position.Positions;
+import com.groupware.note.position.PositionsRepository;
 
 @SpringBootTest
 class NoteApplicationTests {
-
+	@Autowired
+	private PositionsRepository positionsRepository;
+	
+	@Autowired
+	private DepartmentService departmentService;
+	
+	@Autowired
+	private DepartmentRepository departmentRepository;
+	
 	@Test
-	void contextLoads() {
+	void testJpa() {
+		
+		String[] Departmentset = {"HR", "accounting", "Marketing", "temp"};
+		for(String department : Departmentset) {
+			Departments dep = new Departments();
+			dep.setDepartmentName(department);
+			dep.setPoints(0);
+			this.departmentRepository.save(dep);
+		}
+		
+		
+		String[] positionset = {"section chief", "deputy", "worker" , "intern"};
+		
+		for(String position : positionset) {
+			for(String dep1 : Departmentset) {
+				Positions pos = new Positions();
+				pos.setDepartment(departmentService.findBydepartmentName(dep1));
+				pos.setPositionName(position);
+				positionsRepository.save(pos);
+			}
+		}
+		
+		
+		
+		
+		
+		
+//		Departments dmHR = new Departments();
+//			dmHR.setDepartmentName("HR");
+//			dmHR.setPoints(0);
+//			this.departmentRepository.save(dmHR);
+//		Departments dmAcc = new Departments();
+//			dmAcc.setDepartmentName("Accounting");
+//			dmAcc.setPoints(0);
+//			this.departmentRepository.save(dmAcc);
+//		Departments dmMar = new Departments();
+//			dmMar.setDepartmentName("Marketing");
+//			dmMar.setPoints(0);
+//			this.departmentRepository.save(dmMar);
+//		Departments dmTemp = new Departments();
+//			dmTemp.setDepartmentName("Temp");
+//			dmTemp.setPoints(0);
+//			this.departmentRepository.save(dmTemp);
+//		
+		
+			
+		
+		
+		
+		
 	}
+	
+	
+	
 
 }
