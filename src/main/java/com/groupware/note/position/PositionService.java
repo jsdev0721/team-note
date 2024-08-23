a134d05e612f1f98e6297eab61cd75bdfa16f850
park0905 <rux9444@naver.com>
2024-08-23 14:28:16 +0900

    0823 박은영
    회원정보 수정만 됩니다

--- src/main/java/com/groupware/note/position/PositionService.java
+++ src/main/java/com/groupware/note/position/PositionService.java
@@ -1,10 +1,14 @@
 package com.groupware.note.position;
 
+
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
 import com.groupware.note.DataNotFoundException;
+import com.groupware.note.department.Departments;
+import com.groupware.note.user.UserRepository;
+import com.groupware.note.user.Users;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,6 +16,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class PositionService {
 	private final PositionRepository positionRepository;
+	private final UserRepository userRepository;
 
 	public Positions findById(Integer id) {
 		Optional<Positions> _position = this.positionRepository.findById(id);
@@ -20,4 +25,19 @@ public class PositionService {
 		}
 		return _position.get();
 	}
+	public  void updatePosition(Integer userId,Positions positions) {
+		Optional<Users> users = this.userRepository.findById(userId);
+		if(users.isPresent()) {
+			Users user =users.get();
+			user.setPosition(positions);
+			this.userRepository.save(user);
+		}else {throw new DataNotFoundException("데이터가 없습니다");}
+	}
+	public Positions findByPositionName(String positioName,Departments id) {
+		Optional<Positions> positions =this.positionRepository.findByPositionNameAndDepartment(positioName, id);
+		if(positions.isPresent()) {
+			return positions.get();
+		}else {throw new DataNotFoundException("데이터가 없습니다");}
+	}
 }
+
