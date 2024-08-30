4c235e356c3e280feb2f94f4d6330c1de82f00a9
estelsn <cjsdltmdgh@naver.com>
2024-08-30 15:32:55 +0900

    0830 이승호 1차 복지몰 구매내역
    연/월별 구매내역 불러오기 만들어뒀음

--- src/main/java/com/groupware/note/expense/PurchaseDataService.java
+++ src/main/java/com/groupware/note/expense/PurchaseDataService.java
@@ -1,31 +1,100 @@
 package com.groupware.note.expense;
 
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.Year;
+import java.time.YearMonth;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Service;
 
+import com.groupware.note.department.DepartmentRepository;
+import com.groupware.note.department.Departments;
+import com.groupware.note.user.UserDetails;
+import com.groupware.note.user.UserDetailsService;
+import com.groupware.note.user.UserRepository;
+import com.groupware.note.user.Users;
 import com.groupware.note.welfaremall.Purchase;
 import com.groupware.note.welfaremall.PurchaseRepository;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 @Service
 @RequiredArgsConstructor
 public class PurchaseDataService {
 	private final PurchaseRepository pRepo;
-	//전체 데이터
-	public List<Purchase> basicList(){
-		List<Purchase> list = new ArrayList<>();
-		list = this.pRepo.findByPurchaseStatus("complete");
-		return list;
+	private final UserDetailsService udService;
+	private final UserRepository uRepo;
+	private final DepartmentRepository dRepo;
+	
+	@Getter
+	@Setter
+	public class PData{
+		private UserDetails userDetail;
+		private Departments dep;
+		
+		private int year;
+		private int month; 
+		private Integer totalPrice;
 	}
 	
-	//부서별/사람별 데이터
-	public List<Purchase> ListByType(String purchaseType) {
-		List<Purchase> list = new ArrayList<>();
-		list = this.pRepo.findByPurchaseStatusAndPurchaseType("complete", purchaseType);
-		return list;
+	public List<PData> fpcList(String purchaseType){
+		List<PData> list = new ArrayList<>();
+		if(purchaseType.equals("group")) {
+			List<Departments> depList = this.dRepo.findAll();
+			for(Departments d : depList) {
+				List<Purchase> pList = this.pRepo.findByPurchaseStatusAndPurchaseTypeAndDepartment("complete", purchaseType, d);
+				for(int i=2010; i<=LocalDateTime.now().getYear(); i++) {
+				for(int j=1; j<=12; j++) {
+					Integer price = 0; 
+					for(Purchase p : pList) {
+						if(p.getPurchaseDate().getMonthValue()==j && p.getPurchaseDate().getYear()==i) {
+							price = price + p.getTotalPrice();
+							pList.remove(p);
+						}
+					}
+					if(price!=0) {
+						PData pd = new PData();
+						pd.setDep(d);
+						pd.setTotalPrice(price);
+						pd.setYear(i);
+						pd.setMonth(j);
+						list.add(pd);
+					}
+				}	
+				}
+			}
+			return list;
+		} else {
+			List<Users> userList = this.uRepo.findAll();
+			for(Users u : userList) {
+				List<Purchase> pList = this.pRepo.findByPurchaseStatusAndPurchaseTypeAndUser("complete", "personal", u);
+				for(int i=2010; i<=LocalDateTime.now().getYear(); i++) {
+					for(int j=1; j<=12; j++) {
+						Integer price = 0; 
+						for(Purchase p : pList) {
+							if(p.getPurchaseDate().getMonthValue()==j && p.getPurchaseDate().getYear()==i) {
+								price = price + p.getTotalPrice();
+							}
+						}
+						if(price!=0) {
+							PData pd = new PData();
+							pd.setUserDetail(this.udService.findByUser(u));
+							pd.setTotalPrice(price);
+							pd.setYear(i);
+							pd.setMonth(j);
+							list.add(pd);
+							System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+						}
+					}	
+					}
+			}
+			return list;
+		}
 	}
+
 }
- //포인트 입력기능 만들기
