107d39d2100f2cb9e75b6a521ccad1cbd51f77aa
estelsn <cjsdltmdgh@naver.com>
2024-09-01 17:56:29 +0900

    0901 이승호 html 전체 수정, 구매목록 업데이트
    구매목록이랑 메세지 html 수정함.

--- src/main/java/com/groupware/note/expense/PurchaseDataService.java
+++ src/main/java/com/groupware/note/expense/PurchaseDataService.java
@@ -1,9 +1,14 @@
 package com.groupware.note.expense;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
+
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 
 import com.groupware.note.department.DepartmentRepository;
 import com.groupware.note.department.Departments;
@@ -11,6 +16,10 @@ import com.groupware.note.user.UserDetails;
 import com.groupware.note.user.UserDetailsRepository;
 import com.groupware.note.user.UserDetailsService;
 import com.groupware.note.user.UserRepository;
+import com.groupware.note.user.Users;
+import com.groupware.note.welfaremall.Cart;
+import com.groupware.note.welfaremall.CartRepository;
+import static java.time.temporal.TemporalAdjusters.*;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -23,6 +32,7 @@ public class PurchaseDataService {
 	private final UserRepository uRepo;
 	private final UserDetailsRepository udRepo;
 	private final DepartmentRepository dRepo;
+	private final CartRepository cRepo;
 	
 	@Getter
 	@Setter
@@ -35,60 +45,63 @@ public class PurchaseDataService {
 		private Integer totalPrice;
 	}
 	
-//	//구매내역 findPurchaseList
-//	public List<PData> fpcList(String purchaseType){
-//		List<PData> list = new ArrayList<>();
-//		if(purchaseType.equals("group")) {
-//			List<Departments> depList = this.dRepo.findAll();
-//			for(Departments d : depList) {
-//				List<Purchase> pList = this.pRepo.findByPurchaseStatusAndPurchaseTypeAndDepartment("complete", purchaseType, d);
-//				for(int i=2010; i<=LocalDateTime.now().getYear(); i++) {
-//				for(int j=1; j<=12; j++) {
-//					Integer price = 0; 
-//					for(Purchase p : pList) {
-//						if(p.getPurchaseDate().getMonthValue()==j && p.getPurchaseDate().getYear()==i) {
-//							price = price + p.getTotalPrice();
-//						}
-//					}
-//					if(price!=0) {
-//						PData pd = new PData();
-//						pd.setDep(d);
-//						pd.setTotalPrice(price);
-//						pd.setYear(i);
-//						pd.setMonth(j);
-//						list.add(pd);
-//					}
-//				}	
-//				}
-//			}
-//			return list;
-//		} else {
-//			List<Users> userList = this.uRepo.findAll();
-//			for(Users u : userList) {
-//				List<Purchase> pList = this.pRepo.findByPurchaseStatusAndPurchaseTypeAndUser("complete", "personal", u);
-//				for(int i=2010; i<=LocalDateTime.now().getYear(); i++) {
-//					for(int j=1; j<=12; j++) {
-//						Integer price = 0; 
-//						for(Purchase p : pList) {
-//							if(p.getPurchaseDate().getMonthValue()==j && p.getPurchaseDate().getYear()==i) {
-//								price = price + p.getTotalPrice();
-//							}
-//						}
-//						if(price!=0) {
-//							PData pd = new PData();
-//							pd.setUserDetail(this.udService.findByUser(u));
-//							pd.setTotalPrice(price);
-//							pd.setYear(i);
-//							pd.setMonth(j);
-//							list.add(pd);
-//						}
-//					}	
-//					}
-//			}
-//			return list;
-//		}
-//	}
 	
+	
+	//구매내역 findPurchaseList 제일 기본. 부서/개인별
+	public List<PData> fpcList(String type){
+		List<PData> list = new ArrayList<>();
+		if(type.equals("group")) {
+			List<Departments> depList = this.dRepo.findAll();
+			for(Departments d : depList) {
+				List<Cart> cList = this.cRepo.findByStatusAndTypeAndDepOrderByDate("complete", type, d);
+				for(int i=2010; i<=LocalDateTime.now().getYear(); i++) {
+				for(int j=1; j<=12; j++) {
+					Integer price = 0; 
+					for(Cart c : cList) {
+						if(c.getAddDate().getMonthValue()==j && c.getAddDate().getYear()==i) {
+							price = price + c.getPoint();
+						}
+					}
+					if(price!=0) {
+						PData pd = new PData();
+						pd.setDep(d);
+						pd.setTotalPrice(price);
+						pd.setYear(i);
+						pd.setMonth(j);
+						list.add(pd);
+					}
+				}	
+				}
+			}
+			return list;
+		} else {
+			List<Users> userList = this.uRepo.findAll();
+			for(Users u : userList) {
+				List<Cart> cList = this.cRepo.findByStatusAndTypeAndUserOrderByDate("complete", type, u);
+				for(int i=2010; i<=LocalDateTime.now().getYear(); i++) {
+					for(int j=1; j<=12; j++) {
+						Integer price = 0; 
+						for(Cart c : cList) {
+							if(c.getAddDate().getMonthValue()==j && c.getAddDate().getYear()==i) {
+								price = price + c.getPoint();
+							}
+						}
+						if(price!=0) {
+							PData pd = new PData();
+							pd.setUserDetail(this.udService.findByUser(u));
+							pd.setTotalPrice(price);
+							pd.setYear(i);
+							pd.setMonth(j);
+							list.add(pd);
+						}
+					}	
+					}
+			}
+			return list;
+		}
+	}
+	
+	//기본 리스트 가져다가 해당 월에 올라온 데이터만 추출
 	public List<PData> findByDateList(List<PData> list, int year, int month){
 		List<PData> dateList = new ArrayList<>();
 		for(PData p : list) {
@@ -98,7 +111,7 @@ public class PurchaseDataService {
 		}
 		return dateList;
 	}
-	
+	//기본 리스트 가져다가 해당 아이디 별로(dep, personal) 추출
 	public List<PData> findByIdList(List<PData> list, int id, String pt){
 		List<PData> idList = new ArrayList<>();
 		if(pt.equals("group")) {
@@ -121,21 +134,32 @@ public class PurchaseDataService {
 		return idList;
 	}
 	
-	
-//	public List<Purchase> findPurchaseDetailList( int year, int month, int id, String purchaseType){
-//		List<Purchase> list = new ArrayList<>();
-//		LocalDate baseDate = LocalDate.of(year, month, 15);
-//		LocalDateTime startDate = baseDate.with(firstDayOfMonth()).atStartOfDay(); // 00:00:00.00000000
-//	    LocalDateTime endDate = baseDate.with(lastDayOfMonth()).atTime(LocalTime.MAX); // 23:59:59.999999
-//		
-//	    if(purchaseType.equals("personal")) {
-//	    	Users user = this.uRepo.findById(id).get();
-//	    	list = this.pRepo.findByPurchaseStatusAndPurchaseTypeAndUserAndPurchaseDateBetween("complete", purchaseType, user, startDate, endDate);
-//	    } else if(purchaseType.equals("group")) {
-//	    	Departments dep = this.dRepo.findById(id).get();
-//	    	list = this.pRepo.findByPurchaseStatusAndPurchaseTypeAndDepartmentAndPurchaseDateBetween("complete", purchaseType, dep, startDate, endDate);
-//	    } 
-//		return list;
+//	@Setter
+//	@Getter
+//	public class CartDataDetail {
+//		private UserDetails ud;
+//		private List<Cart> cartList;
 //	}
-
+	
+	//해당 월의 사원/부서 구매목록 전체
+	public List<Cart> findPurchaseDetailList(Model model, int year, int month, int id, String purchaseType) {
+		List<Cart> list = new ArrayList<>();
+		LocalDate baseDate = LocalDate.of(year, month, 15);
+		LocalDateTime startDate = baseDate.with(firstDayOfMonth()).atStartOfDay(); // 00:00:00.00000000
+	    LocalDateTime endDate = baseDate.with(lastDayOfMonth()).atTime(LocalTime.MAX); // 23:59:59.999999
+	    String name = "오류";
+	    if(purchaseType.equals("personal")) {
+	    	Users user = this.uRepo.findById(id).get();
+	    	 name= this.udRepo.findById(id).get().getName();
+	    	list = this.cRepo.findByStatusAndTypeAndUserAndAddDateBetween("complete", purchaseType, user, startDate, endDate);
+	    	String details = user.getPosition().getPositionName() + "/" +user.getPosition().getDepartment().getDepartmentName();
+	    	model.addAttribute("details", details);
+	    } else if(purchaseType.equals("group")) {
+	    	Departments dep = this.dRepo.findById(id).get();
+	    	name = dep.getDepartmentName();
+	    	list = this.cRepo.findByStatusAndTypeAndDepAndDateBetOrderBydate("complete", purchaseType, dep, startDate, endDate);
+	    } 
+	    model.addAttribute("titleName", name);
+		return list;
+	}
 }
--- src/main/java/com/groupware/note/welfaremall/Cart.java
+++ src/main/java/com/groupware/note/welfaremall/Cart.java
@@ -29,16 +29,16 @@ public class Cart {
 	@ManyToOne
 	@JoinColumn(name = "product_id")
 	private WelfareMall product;
-	
+	//양
 	private Integer quantity;
-	
+	//가격
 	private Integer point;
 	
 	private LocalDateTime addDate;
-	
+	//개인/부서
 	private String type;
-	
+	//옵션(템 종류)
 	private String option;
-	
+	//queue/process/complete
 	private String status;
 }
