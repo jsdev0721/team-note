27df890f19af9948480597619e7ab22e71bdada7
estelsn <cjsdltmdgh@naver.com>
2024-09-12 14:36:42 +0900

    0912 이승호
    필요 없는 코드 삭제

--- src/main/java/com/groupware/note/welfaremall/CartRepository.java
+++ src/main/java/com/groupware/note/welfaremall/CartRepository.java
@@ -27,9 +27,6 @@ public interface CartRepository extends JpaRepository<Cart, Integer> {
 	@Query("select c from Cart c where c.status= :status and c.type= :type and c.user.position.department= :department and c.addDate > :start AND c.addDate <= :end")
 	List<Cart> findByStatusAndTypeAndDepAndAddDateBetween(@Param("status") String status,  @Param("type") String type,  @Param("department") Departments department, @Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
 	
-	@Query("select c from Cart c where c.status= :status and c.type= :type and c.user = :user and c.addDate > :start AND c.addDate <= :end ")
-	List<Cart> findByStatusAndTypeAndUserAndDateBet(@Param("status") String status,  @Param("type") String type,  @Param("user") Users user,  @Param("start") LocalDateTime start, @Param("end") LocalDateTime end  );
-	
 	List<Cart> findByStatusAndTypeAndUserAndAddDateBetween(String status, String type, Users user, LocalDateTime start, LocalDateTime end);
 	
 	@Query("select c FROM Cart c WHERE c.type= :type and c.status= :status and c.user.position.department= :department and c.addDate > :start AND c.addDate <= :end order by c.addDate desc")
